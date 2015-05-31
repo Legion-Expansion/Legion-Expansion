0ea63261dce5e0f375d72f4909b48dcd6a7f4afd
KillerKiwiJuice <eoin@comcast.net>
2015-05-31 13:12:48 -0400

    added boom bot

--- ui/main/shared/js/build.js
+++ ui/main/shared/js/build.js
@@ -62,6 +62,7 @@ function BuildHotkeyModel() {
             "/pa/units/land/bot_sniper/bot_sniper.json": ["bot", 7],
             "/pa/units/land/fabrication_bot_combat_adv/fabrication_bot_combat_adv.json": ["bot", 8],
             "/pa/units/land/bot_bomb/bot_bomb.json": ["bot", 14],
+            "/pa/units/land/L_bot_bomb/L_bot_bomb.json": ["bot", 4],
             "/pa/units/land/fabrication_bot/fabrication_bot.json": ["bot", 10],
             "/pa/units/land/assault_bot/assault_bot.json": ["bot", 11],
             "/pa/units/land/bot_grenadier/bot_grenadier.json": ["bot", 12],
