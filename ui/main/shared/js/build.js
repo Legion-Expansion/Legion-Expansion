f3ef6049182dc25b680e44509949b2664aef5941
KillerKiwiJuice <eoin@comcast.net>
2015-06-03 20:19:28 -0400

    fixed shotgun tank and radar.

--- ui/main/shared/js/build.js
+++ ui/main/shared/js/build.js
@@ -50,9 +50,10 @@ function BuildHotkeyModel() {
 
 
             "/pa/units/land/fabrication_vehicle_adv/fabrication_vehicle_adv.json": ["vehicle", 5],
-            //"/pa/units/land/tank_shank/tank_shank.json": ["vehicle", 9],
+            "/pa/units/land/tank_shank/tank_shank.json": ["vehicle", 9],
             "/pa/units/land/tank_laser_adv/tank_laser_adv.json": ["vehicle", 6],
             "/pa/units/land/tank_heavy_armor/tank_heavy_armor.json": ["vehicle", 7],
+            "/pa/units/land/L_tank_heavy_armor/L_tank_heavy_armor.json": ["vehicle", 2],
             "/pa/units/land/tank_heavy_mortar/tank_heavy_mortar.json": ["vehicle", 8],
             "/pa/units/land/fabrication_vehicle/fabrication_vehicle.json": ["vehicle", 10],
             "/pa/units/land/L_fabrication_vehicle/L_fabrication_vehicle.json": ["vehicle", 3],
