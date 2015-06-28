c8332ac9631a778c6158bde4fbfcf5380fd27d1a
KillerKiwiJuice <eoin@comcast.net>
2015-06-28 00:49:05 -0400

    i don't even remember what i did

--- pa/original_models/auto-papa.bat
+++ pa/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\tele_energy_poly.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\tele_energy_poly.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\L_teleporter.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\L_teleporter.fbx"
 pause
