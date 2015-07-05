c9b2aa2aaac0adaf3e1f94cb19b76ebe98d4733f
KillerKiwiJuice <eoin@comcast.net>
2015-07-05 12:37:01 -0400

    fixed things, added rocket bot

--- pa/original_models/auto-papa.bat
+++ pa/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\L_bot_bomb_walk.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\L_bot_bomb_walk.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\L_bot_bomb.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\pa\original_models\L_bot_bomb.fbx"
 pause
