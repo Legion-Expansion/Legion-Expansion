ded37f786f9b95d1fcaa2b2d25ed24e873ebe990
KillerKiwiJuice <eoin@comcast.net>
2015-07-15 00:37:52 -0400

    button shows in lobby
    it has no functionality

--- ui/mods/new_game.js
+++ ui/mods/new_game.js
@@ -1,10 +1,10 @@
-(function () {
-/////////////////
+function kLobbyAdditions() {
+/*    /////////////////
     //lock shared army mechanism **HIGHLY WIP**
-/////////////////
-/*  var buildNumber = decode(sessionStorage['build_version']);
+    /////////////////
+    var buildNumber = decode(sessionStorage['build_version']);
 
-	console.log("Currently Running Legion Faction Mod on " + version + ", last tested on 82834");
+    console.log("Currently Running Legion Faction Mod on " + version + ", last tested on 82834");
 
     var legionFaction = {};
 
@@ -15,17 +15,14 @@
     ];
     var chosenCommander = ko.observable(legionFaction.newCommanders[0]);
     var lockArmy = function (chosenCommander) {
-        if (chosenCommander === 0 //a legion faction commander, the 0 is in place as a placeholder ) {
-            //prevent other players in the army from choosing a vanilla commander
+        if (chosenCommander === "a legion faction commander" ) {
+            //lock the army to legion commanders
         }
     };
-    if (self.toggleShareArmy()) {
-
-    }
 */
-/////////////////
+    /////////////////
     //button
-/////////////////
+    /////////////////
     model.factionEnabled = ko.observable(false);
     //Toggle the boolean value of 'factionEnabled' when clicked via knockout button
     model.toggleFactionEnabled = function () {
@@ -36,16 +33,13 @@
     model.buttonText = ko.computed(function () {
         return model.factionEnabled() ? "Disable Legion Faction" : "Enable Legion Faction";
     });
-    model.sendChatMessage = function (message) {
-        var msg = {};
-        msg.message = message;
-        model.send_message("chat_message", msg);
-    };
-    //send chat message when the observable changes boolean values
-    if (model.factionEnabled) {
-        model.sendChatMessage(player_name + "has enabled Legion faction");
-    } else {}
 
     $("#game-bar").append('<div class="btn_std" id="enable_faction" data-bind="click: toggleFactionEnabled, css: { btn_hero: factionEnabled}" style="margin: 0px -15px;"><div class="btn_label" style="width: 200px;font-size:15px;" data-bind="text: buttonText"><div style="font-size:15px;"></div></div></div>');
 
-})();
+}
+kLobbyAdditions();
+try {
+    kLobbyAdditions();
+} catch (i) {
+    console.log(i);
+}
