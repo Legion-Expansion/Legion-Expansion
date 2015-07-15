ca743b92087d33f55a38e3cbbd2d342aa1d15ac0
KillerKiwiJuice <eoin@comcast.net>
2015-07-14 20:10:39 -0400

    minor update, not even sure if this is worth it

--- ui/mods/new_game.js
+++ ui/mods/new_game.js
@@ -1,13 +1,22 @@
-var legionFaction = { };
+(function () {
+	var buildNumber = decode(sessionStorage['build_version']);
 
-legionFaction.newCommanders = [
-    "TODO1",
-    "TODO2"
+	console.log("Currently Running Legion Faction Mod on " + version + ", last tested on 82834");
+
+    var legionFaction = {};
+
+    legionFaction.newCommanders = [
+        "TODO1",
+        "TODO2"
     //etc
-]
-var performLockdown = function() {
-    
-}
-if (self.toggleShareArmy()) {
-    
-}
+    ];
+    var chosenCommander = ko.observable(legionFaction.newCommanders[0]);
+    var lockArmy = function (chosenCommander) {
+        if (chosenCommander === 0 /*a legion faction commander, the 0 is in place as a placeholder*/ ) {
+            /*prevent other players in the army from choosing a vanilla commander*/
+        }
+    };
+    if (self.toggleShareArmy()) {
+
+    }
+})();
