365d2ec0222943fc95a2fe8561da4a3b0316f55e
KillerKiwiJuice <eoin@comcast.net>
2015-07-15 16:55:16 -0400

    New game update

--- ui/mods/new_game.js
+++ ui/mods/new_game.js
@@ -6,23 +6,38 @@ function kLobbyAdditions() {
 
     console.log("Currently Running Legion Faction Mod on " + version + ", last tested on 82834");
 
-    var legionFaction = {};
+    $.ajax('coui://pa/units/unit_list.json', {
+        cache: false,
+        async: false,
+        dataType: 'json'
+    }).done(function (data) {
+        units_json = data.units;
+    });
 
-    legionFaction.newCommanders = [
-        "TODO1",
-        "TODO2"
-    //etc
-    ];
-    var chosenCommander = ko.observable(legionFaction.newCommanders[0]);
-    var lockArmy = function (chosenCommander) {
-        if (chosenCommander === "a legion faction commander" ) {
-            //lock the army to legion commanders
-        }
-    };
+    if (model.factionEnabled) {
+        //Restrict UI to legion units
+    } else {
+        //Restrict UI to vanilla units
+    }
+}
 */
-    /////////////////
-    //button
-    /////////////////
+/////////////////
+//AI dropdown mechanism
+/////////////////
+
+(function () {
+    model.legionOptions = ko.observableArray([
+        "Vanilla",
+        "Legion"]);
+    model.defaultLegionOption = ko.observable(model.legionOptions()[0]);
+    $(".form-control-personality").after('<div class="form-control-personality"><select data-bind="options: model.legionOptions, selectPicker: model.defaultLegionOption, visible: model.isGameCreator" data-width="106px"></select></div>');
+})();
+
+/////////////////
+//button
+/////////////////
+
+(function () {
     model.factionEnabled = ko.observable(false);
     //Toggle the boolean value of 'factionEnabled' when clicked via knockout button
     model.toggleFactionEnabled = function () {
@@ -36,10 +51,7 @@ function kLobbyAdditions() {
 
     $("#game-bar").append('<div class="btn_std" id="enable_faction" data-bind="click: toggleFactionEnabled, css: { btn_hero: factionEnabled}" style="margin: 0px -15px;"><div class="btn_label" style="width: 200px;font-size:15px;" data-bind="text: buttonText"><div style="font-size:15px;"></div></div></div>');
 
-}
-kLobbyAdditions();
-try {
-    kLobbyAdditions();
-} catch (i) {
-    console.log(i);
-}
+    console.log("Legion button successfully loaded");
+})();
+
+//kLobbyAdditions();
