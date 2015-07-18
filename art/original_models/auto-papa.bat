2871aeb78bb7ba895a97bb2ab344afe678a2991d
KillerKiwiJuice <eoin@comcast.net>
2015-07-18 19:42:05 -0400

    added new dev commander

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\art\original_models\overwatch_commander.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\art\original_models\overwatch_commander.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\art\original_models\laser_beam_mesh.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\art\original_models\laser_beam_mesh.fbx"
 pause
