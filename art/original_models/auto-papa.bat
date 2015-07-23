5a2e34de4e8b591014379471eefe8a66f7990e3e
KillerKiwiJuice <eoin@comcast.net>
2015-07-23 14:21:54 -0400

    Large Update

---
+++ art/original_models/auto-papa.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\art\original_models\L_bot_factory_build.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.brokenalliance\art\original_models\L_bot_factory_build.fbx"
+pause
---
+++ ui/mods/com.pa.legionfaction/new_game.js
@@ -0,0 +1,59 @@
+var legionFactionLoaded;
+
+if ( ! legionFactionLoaded )
+{
+
+    legionFactionLoaded = true;
+    
+    function legionFaction()
+    {
+    
+        var buildVersion = decode( sessionStorage.build_version );
+    
+        var patchName = 'legionFaction new_game.js';
+    
+        console.log(patchName + ' on ' + buildVersion + ' last tested on 83796');
+        
+        var commanderObjectNameToHack = 'ImperialFiveleafclover'
+        
+        var commanderIdToHack = '/pa/units/commanders/imperial_fiveleafclover/imperial_fiveleafclover.json';
+        
+       if ( buildVersion == 83796 )
+       {
+            var catalog = model.extendedCatalog();
+            
+            var commanderToHack = _.find( catalog, { ObjectName: commanderObjectNameToHack } );
+            
+            commanderToHack.IsFree = true;
+            
+            commanderToHack.DisplayName = "Legion Commander";
+            
+            model.extendedCatalog( catalog );
+                
+        }
+        else
+        {
+            model.commanders.subscribe( function( commanders )
+            {
+                commanders.push( commanderIdToHack );
+            });
+        }
+        
+        model.selectedCommander.subscribe( function( selectedCommander )
+        {
+            
+            localStorage.setItem( 'legionFactionSelected', selectedCommander == commanderIdToHack );
+           
+        } );
+    }
+    
+    try
+    {
+        legionFaction();
+    }
+    catch (e)
+    {
+        console.log(e);
+        console.log(JSON.stringify(e));
+    }
+}
