5022fcf68beb30282454e14e18ba92d11feb12a8
KillerKiwiJuice <eoin@comcast.net>
2015-07-24 19:11:45 -0400

    New DDS bottom turret effect

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_orbital_laser.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_orbital_laser.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_orbital_fighter_open.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_orbital_fighter_deploy.fbx"
 pause
