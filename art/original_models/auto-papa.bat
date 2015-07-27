845ecf9c41308a9424bbe19a843d39758c709fdb
KillerKiwiJuice <eoin@comcast.net>
2015-07-27 14:08:52 -0400

    Lots of tweaks + vehicle factory

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_rocket_barrage.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_rocket_barrage.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_vehicle_factory_build.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_vehicle_factory_build.fbx"
 pause
