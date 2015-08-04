851b9a2781d460d074bbc5f25d12cc068d37f15b
KillerKiwiJuice <eoin@comcast.net>
2015-08-03 22:29:31 -0400

    Mesh Bounds, Tele fix, orbital fighter

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,3 +1,4 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_orbital_laser_open.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_orbital_laser_open.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_main.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_main.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_secondary.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_secondary.fbx"
 pause
