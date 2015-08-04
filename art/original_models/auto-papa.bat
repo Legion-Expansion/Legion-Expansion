45bcb6aa572fbe3974a2321aa35c7bd465e378ef
KillerKiwiJuice <eoin@comcast.net>
2015-08-04 18:47:14 -0400

    3DSFiles, effects, tweaks, cleaning

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,4 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_main.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_main.fbx"
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_secondary.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\sun_flare_secondary.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_vehicle_factory.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_vehicle_factory.fbx"
 pause
