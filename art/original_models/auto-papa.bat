885d4404a981af5fa3d7495d583cb563e91d2e95
KillerKiwiJuice <eoin@comcast.net>
2015-08-06 11:57:32 -0400

    Large Sync + Update

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_vehicle_factory.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_vehicle_factory.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_tele_ring.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legionfaction.server\art\original_models\L_tele_ring.fbx"
 pause
