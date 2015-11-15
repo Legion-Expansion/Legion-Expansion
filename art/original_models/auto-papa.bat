42950d5dd3b29a272b6123134e6808f0a2211cc4
KillerKiwiJuice <eoin@comcast.net>
2015-11-15 11:38:02 -0500

    Add BFG

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,4 +1,4 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_flying_teleporter.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_flying_teleporter.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_bot_artillery_adv.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_bfg_bot.fbx"
 pause
