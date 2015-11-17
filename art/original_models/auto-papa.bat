c7f534a183428003de5a4cf33a9a576ac685df47
KillerKiwiJuice <eoin@comcast.net>
2015-11-17 16:14:34 -0500

    Add t1 aa boat

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,4 +1,4 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_bot_artillery_adv.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_bfg_bot.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_frigate.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_frigate.fbx"
 pause
