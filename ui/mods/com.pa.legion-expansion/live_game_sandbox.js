f29c6b0f6ee66ba5ad540762ee4028b85bd5384a
Justin Love <git@JustinLove.name>
2015-11-25 12:44:34 -0600

    add extensions for Sandbox Unit Organizer 1.6+

---
+++ ui/mods/com.pa.legion-expansion/live_game_sandbox.js
@@ -0,0 +1,47 @@
+var legionExpansionLoaded;
+
+if ( ! legionExpansionLoaded )
+{
+
+    legionExpansionLoaded = true;
+
+    function legionExpansion()
+    {
+
+        var buildVersion = decode( sessionStorage.build_version );
+
+        var patchName = 'legionExpansion live_game_sandbox.js';
+
+        console.log(patchName + ' on ' + buildVersion + ' last tested on 89755');
+
+        if (model.baseGroups) {
+            model.baseGroups.splice(99, 0, 
+                'L_factory',
+                'L_combat',
+                'L_utility',
+                'L_orbital_structure',
+                'L_ammo')
+        }
+        if (model.mobileGroups) {
+            model.mobileGroups.splice(99, 0, 
+                'L_vehicle',
+                'L_bot',
+                'L_air',
+                'L_sea',
+                'L_orbital')
+        }
+        if (model.miscUnits) {
+            model.miscUnits.push('/pa/units/commanders/imperial_fiveleafclover/imperial_fiveleafclover.json')
+        }
+    }
+
+    try
+    {
+        legionExpansion();
+    }
+    catch (e)
+    {
+        console.log(e);
+        console.log(JSON.stringify(e));
+    }
+}
