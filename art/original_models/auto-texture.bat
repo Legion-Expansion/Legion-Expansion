a9b2611bfcd7cdd9048df0ece7f9d28b4fff8eac
KillerKiwiJuice <eoin@comcast.net>
2015-11-29 13:11:22 -0500

    Update radar

---
+++ art/original_models/auto-texture.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_sniper_bot_diffuse.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\L_sniper_bot_diffuse.png"
+pause
