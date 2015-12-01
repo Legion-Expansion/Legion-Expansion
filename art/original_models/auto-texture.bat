0df1d3b1cbb55a135f751b1003bbd294e8abaae5
KillerKiwiJuice <eoin@comcast.net>
2015-11-30 22:20:19 -0500

    Lancer Bot textures

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,3 +1,5 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_sniper_bot_diffuse.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\L_sniper_bot_diffuse.png"
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_sniper_bot_material.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\L_sniper_bot_material.png"
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_sniper_bot_mask.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\L_sniper_bot_mask.png"
 pause
