38135c620ceed811e1f53fe65f7939c255aa497f
KillerKiwiJuice <eoin@comcast.net>
2015-12-02 13:41:35 -0500

    Adv Vehicle Fac texture (first pass)

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_sniper_bot.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_sniper_bot.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_vehicle_factory_adv.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_vehicle_factory_adv.fbx"
 pause
