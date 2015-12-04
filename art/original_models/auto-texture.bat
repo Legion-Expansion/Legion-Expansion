f8e53afb59c6ae8a87686db10a19f58696899add
KillerKiwiJuice <eoin@comcast.net>
2015-12-04 14:38:45 -0500

    Adv Radar Textures, better auto texture

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,5 +1,11 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
-papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_bot_artillery_adv_diffuse.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\L_bot_artillery_adv_diffuse.png"
-papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_bot_artillery_adv_material.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\L_bot_artillery_adv_material.png"
-papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_bot_artillery_adv_mask.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\L_bot_artillery_adv_mask.png"
+
+set unit=L_radar_adv_
+set diffuse=%unit%diffuse
+set material=%unit%material
+set mask=%unit%mask
+
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%diffuse%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\%diffuse%.png"
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%material%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\%material%.png"
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%mask%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\%mask%.png"
 pause
