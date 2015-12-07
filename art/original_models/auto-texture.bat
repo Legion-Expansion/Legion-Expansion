af0aa77bb3c3951df6ddab4bc4816daa8c5fe7fb
KillerKiwiJuice <eoin@comcast.net>
2015-12-06 19:25:40 -0500

    Fix textures part one

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_ion_defense_
+set unit=L_t1_turret_adv_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
