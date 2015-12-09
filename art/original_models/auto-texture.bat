1468a84de379eafac39d47e75d2db6810c137dd4
KillerKiwiJuice <eoin@comcast.net>
2015-12-09 18:12:53 -0500

    Texture compression

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -5,7 +5,7 @@ set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
 
-papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%diffuse%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\%diffuse%.png"
-papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%material%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\%material%.png"
-papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%mask%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa\effects\textures\particles\uncompressed\%mask%.png"
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%diffuse%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa_ex1\units\land\unit_cannon\%diffuse%.png"
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%material%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa_ex1\units\land\unit_cannon\%material%.png"
+papatran --texture-mode include -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%mask%.papa" "C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\media\pa_ex1\units\land\unit_cannon\%mask%.png"
 pause
