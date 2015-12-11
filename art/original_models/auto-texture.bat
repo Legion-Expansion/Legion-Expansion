bbe071be5f50f4ba49c55b25b5d583aebfd87f27
KillerKiwiJuice <eoin@comcast.net>
2015-12-10 19:17:15 -0500

    Air scout + bot fac textures

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_vehicle_factory_adv_
+set unit=L_bot_factory_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
