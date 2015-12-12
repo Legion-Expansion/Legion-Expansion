a8359aa0b85c8f327df9a21b347e8a4d710b7f99
KillerKiwiJuice <eoin@comcast.net>
2015-12-12 17:15:06 -0500

    balance, textures, tweaks

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_tank_shank_
+set unit=L_nuke_launcher_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
