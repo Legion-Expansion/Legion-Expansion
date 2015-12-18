4a6ab70a1029ab2898d8f9c4fc3f904303056078
KillerKiwiJuice <eoin@comcast.net>
2015-12-17 19:05:34 -0500

    Swarm tank

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
-papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_frigate.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_frigate.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_tank_swarm.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\L_swarm_tank.fbx"
 pause
