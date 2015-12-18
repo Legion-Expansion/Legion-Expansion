e2f1807212c85119ecd28a74baa69ae7080b121b
KillerKiwiJuice <eoin@comcast.net>
2015-12-18 17:26:03 -0500

    textures

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_swarm_tank_
+set unit=tank_aeson_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
