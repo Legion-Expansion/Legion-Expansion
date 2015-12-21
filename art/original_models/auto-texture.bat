410210b2918d548dde4249ba1aac4724d444ea45
KillerKiwiJuice <eoin@comcast.net>
2015-12-20 22:51:21 -0500

    Radar textures, tweaks

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=tank_aeson_
+set unit=L_radar_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
