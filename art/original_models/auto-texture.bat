e56b4d6d763c2311e80652aba8174d4d546d1a18
KillerKiwiJuice <eoin@comcast.net>
2016-01-02 20:41:41 -0500

    update textures #1

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_naval_factory_adv_
+set unit=L_bot_factory_adv_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
