7ec3e9b489a8e2e922e5194c44c6068e4ca49234
KillerKiwiJuice <eoin@comcast.net>
2016-01-05 18:03:32 -0500

    format, missile ship

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,4 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
-set unit_to_convert=L_fabrication_sub_combat_adv
+set unit_to_convert=L_missile_ship
 papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_open.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_open.fbx"
+papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_deploy.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_deploy.fbx"
 pause
