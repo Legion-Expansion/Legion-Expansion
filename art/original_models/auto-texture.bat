6e374d8560657bb893376c8233a7f3f2b3925f40
KillerKiwiJuice <eoin@comcast.net>
2016-01-22 20:08:00 -0500

    Better sniper tank

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_destroyer_
+set unit=L_sniper_tank_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
