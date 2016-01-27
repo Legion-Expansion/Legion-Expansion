5b2c5792580bcd7d8a92d8a39ecdc780135144e8
KillerKiwiJuice <eoin@comcast.net>
2016-01-26 23:11:50 -0500

    adjusted bot fac

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_fabrication_ship_
+set unit=L_bot_factory_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
