4a9e4fc9135d6bbfe4979b32aaf96ec8587d36ec
KillerKiwiJuice <eoin@comcast.net>
2016-01-29 15:11:58 -0500

    Raptor support commander
    animations not final

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_nuclear_missile_
+set unit=L_bot_support_commander_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
