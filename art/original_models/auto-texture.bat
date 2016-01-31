378f385710255c745613f67b5c3cb5705e6bd594
KillerKiwiJuice <eoin@comcast.net>
2016-01-30 23:23:44 -0500

    new aa textures, fixed a bug

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_sea_tank_
+set unit=L_air_defense_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
