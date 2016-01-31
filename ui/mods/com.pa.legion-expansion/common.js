360e48f28a6b8fb3aacb6dd3d03cfdb155a0beff
KillerKiwiJuice <eoin@comcast.net>
2016-01-31 12:56:43 -0500

    more css - no me gusta

--- ui/mods/com.pa.legion-expansion/common.js
+++ ui/mods/com.pa.legion-expansion/common.js
@@ -2,8 +2,8 @@
 var legionglobal = {};
 
 legionglobal.commanders = ["/pa/units/commanders/tank_aeson/tank_aeson.json","/pa/units/commanders/imperial_invictus/imperial_invictus.json"];
-legionglobal.basicfactories = ["/pa/units/air/L_air_factory/L_air_factory.json","/pa/units/land/L_bot_factory/L_bot_factory.json"];
-legionglobal.advfactories = [];
+legionglobal.basicfactories = ["/pa/units/air/L_air_factory/L_air_factory.json","/pa/units/land/L_bot_factory/L_bot_factory.json","/pa/units/land/L_vehicle_factory/L_vehicle_factory.json","/pa/units/sea/L_naval_factory/L_naval_factory.json","/pa/units/orbital/L_orbital_launcher/L_orbital_launcher.json"];
+legionglobal.advfactories = ["/pa/units/air/L_air_factory_adv/L_air_factory_adv.json","/pa/units/land/L_bot_factory_adv/L_bot_factory_adv.json","/pa/units/land/L_vehicle_factory_adv/L_vehicle_factory_adv.json","/pa/units/sea/L_naval_factory_adv/L_naval_factory_adv.json","/pa/units/orbital/L_orbital_factory/L_orbital_factory.json"];
 legionglobal.airfabbers = [];
 legionglobal.vehfabbers = [];
 
