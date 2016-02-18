ae89a85968d3917d4de9f14ede019731d5a0f79c
pieter vanh <pieter.vanh@gmail.com>
2016-02-18 17:20:12 +0100

    Legion UI: following js standards

--- ui/mods/com.pa.legion-expansion/live_game_players.js
+++ ui/mods/com.pa.legion-expansion/live_game_players.js
@@ -1,74 +1,104 @@
-//LOAD CUSTOM LEGION NEW GAME CSS
-loadCSS("coui://ui/mods/com.pa.legion-expansion/css/legion_players.css");
-loadScript("coui://ui/mods/com.pa.legion-expansion/common.js");
 
-//see global.js
-var legioncomms = legionglobal.commanders;
+var legionExpansionLoaded;
 
-model.isLegionOrMixedOrVanilla = ko.computed(function () {
-  try{
-    var legioncount = 0;
-    var specslength = 0;
-    var selectedspecs = model.player().commanders;
-    if (selectedspecs !== undefined){
-      _.forOwn(selectedspecs, function(value, key){
-        if(_.includes(legioncomms, value)){
-          legioncount++;
+if ( ! legionExpansionLoaded )
+{
+
+    legionExpansionLoaded = true;
+
+    function legionExpansion()
+    {
+
+        var buildVersion = decode( sessionStorage.build_version );
+
+        var patchName = 'legionExpansion live_game_players.js';
+
+        console.log(patchName + ' on ' + buildVersion + ' last tested on 89755');
+        
+        //LOAD CUSTOM LEGION CSS
+        loadCSS("coui://ui/mods/com.pa.legion-expansion/css/legion_players.css");
+        loadScript("coui://ui/mods/com.pa.legion-expansion/common.js");
+
+        //see global.js
+        var legioncomms = legionglobal.commanders;
+
+        model.isLegionOrMixedOrVanilla = ko.computed(function () {
+        try{
+            var legioncount = 0;
+            var specslength = 0;
+            var selectedspecs = model.player().commanders;
+            if (selectedspecs !== undefined){
+            _.forOwn(selectedspecs, function(value, key){
+                if(_.includes(legioncomms, value)){
+                legioncount++;
+                }
+                specslength++; 
+            });
+            if(legioncount == specslength){
+                return "legion";
+            }
+            else{
+                if(legioncount > 0 && legioncount < specslength){
+                return "mixed";
+                }
+                else{
+                return "vanilla";
+                }
+            }
+            }
+            else{
+            return "vanilla";
+            }
         }
-        specslength++; 
-      });
-      if(legioncount == specslength){
-        return "legion";
-      }
-      else{
-        if(legioncount > 0 && legioncount < specslength){
-          return "mixed";
+        catch(e){
+            return "";
+        }
+        });
+
+        model.isLegion = ko.computed(function (){
+        if(model.isLegionOrMixedOrVanilla() === "legion"){
+            return true;
         }
         else{
-          return "vanilla";
+            return false;
         }
-      }
+        });
+
+        model.isMixed = ko.computed(function (){
+        if(model.isLegionOrMixedOrVanilla() === "mixed"){
+            return true;
+        }
+        else{
+            return false;
+        }
+        });
+
+        model.legionstart = ko.observable(false);
+
+        model.player.subscribe(function(newval){
+            if(!model.legionstart()){
+                var ui = model.isLegionOrMixedOrVanilla();
+                api.Panel.message("selection","legionui", ui);
+                api.Panel.message("planets","legionui", ui);
+                api.Panel.message("control_group_bar","legionui", ui);
+                api.Panel.message("econ","legionui", ui);
+                api.Panel.message("options_bar","legionui", ui);
+                api.Panel.message("build_hover","legionui", ui);
+                model.legionstart(true);
+            }
+        });
+        
+        $('.body_panel').attr("data-bind","css: { legionui: model.isLegion(), mixedui: model.isMixed()}, visible: show");
+
     }
-    else{
-      return "vanilla";
+
+    try
+    {
+        legionExpansion();
     }
-  }
-  catch(e){
-    return "";
-  }
-});
-
-model.isLegion = ko.computed(function (){
- if(model.isLegionOrMixedOrVanilla() === "legion"){
-    return true;
- }
- else{
-    return false;
- }
-});
-
-model.isMixed = ko.computed(function (){
- if(model.isLegionOrMixedOrVanilla() === "mixed"){
-    return true;
- }
- else{
-    return false;
- }
-});
-
-model.legionstart = ko.observable(false);
-
-model.player.subscribe(function(newval){
-    if(!model.legionstart()){
-        var ui = model.isLegionOrMixedOrVanilla();
-        api.Panel.message("selection","legionui", ui);
-        api.Panel.message("planets","legionui", ui);
-        api.Panel.message("control_group_bar","legionui", ui);
-        api.Panel.message("econ","legionui", ui);
-        api.Panel.message("options_bar","legionui", ui);
-        api.Panel.message("build_hover","legionui", ui);
-        model.legionstart(true);
+    catch (e)
+    {
+        console.log(e);
+        console.log(JSON.stringify(e));
     }
-});
-$('.body_panel').attr("data-bind","css: { legionui: model.isLegion(), mixedui: model.isMixed()}, visible: show");
-console.log("players legion");
+}
--- ui/mods/com.pa.legion-expansion/new_game.js
+++ ui/mods/com.pa.legion-expansion/new_game.js
@@ -11,6 +11,74 @@ if (!legionExpansionLoaded) {
         var patchName = 'legionExpansion new_game.js';
 
         console.log(patchName + ' on ' + buildVersion + ' last tested on 85138');
+        
+        //LOAD CUSTOM LEGION CSS
+        loadCSS("coui://ui/mods/com.pa.legion-expansion/css/new_game.css");
+        loadScript("coui://ui/mods/com.pa.legion-expansion/common.js");
+
+        var legioncommanders = legionglobal.commanders;
+        var vanillacommanders = ["/pa/units/commanders/quad_osiris/quad_osiris.json","/pa/units/commanders/imperial_delta/imperial_delta.json"];
+
+        model.isNotLegion = function(commander,isEmpty){
+            if(!isEmpty){
+                return !_.includes(legioncommanders, commander);
+            }
+            else{
+                return true;
+            }
+        }
+
+        //Style Commander Picker Legion
+        $('.div-commander-picker-item.btn_std_ix').attr("data-bind","css: {legioncommander: !model.isNotLegion($data)}, click: function () { model.setCommander($index()) }, click_sound: 'default', rollover_sound: 'default'");
+
+        //Style Slot Legion
+        $('.slot-player').attr("data-bind","css: {legionslot: !model.isNotLegion($data.commander(),$data.isEmpty()), ready: isReady, loading: isLoading}");
+
+
+        model.changeLegionAI = function(playerid){
+            //console.log("change to legion");
+            model.send_message('set_ai_commander', {
+                id: playerid,
+                ai_commander: legioncommanders[_.random(legioncommanders.length-1)]
+            });   
+        }
+
+        model.changeVanillaAI = function(playerid){
+            //console.log("change to vanilla");
+            model.send_message('set_ai_commander', {
+                id: playerid,
+                ai_commander: vanillacommanders[_.random(vanillacommanders.length-1)]
+            });   
+        }
+
+        //PATCHED lobby.js
+        //To Legion Button
+        //$('.army-button.slot-remove-button.slot-remove-button-team').parent().append('<div class="army-button btn_add_ai" data-bind="visible: slot.ai() && model.isNotLegion(slot.commander()),click: function() { model.changeLegionAI(slot.playerId());}">to Legion</div>');
+        //To Vanilla Button
+        //$('.army-button.slot-remove-button.slot-remove-button-team').parent().append('<div class="army-button btn_add_ai" data-bind="visible: slot.ai() && !model.isNotLegion(slot.commander()),click: function() { model.changeVanillaAI(slot.playerId());}">to Vanilla</div>');
+        //ENDOF PATCHED lobby.js
+        
+        //UNPATCHED lobby.js
+        //HACKY WAY TO SWITCH LEGION COMMANDER till lobby.js is updated
+        model.LegionCommander = function (index) {
+            model.targetAIArmyIndex(index);
+            model.targetAISlotIndex(0);
+            model.addAI();
+            console.log(index);
+            setTimeout(model.CheckLegionCommander, 200, index);
+        };
+        model.CheckLegionCommander = function (index) {
+            if ((model.armies()[index].slots()[0].commander() !== "/pa/units/commanders/tank_aeson/tank_aeson.json") && (model.armies()[index].slots()[0].commander() !== "/pa/units/commanders/imperial_invictus/imperial_invictus.json")) {
+                console.log(model.armies()[index].slots()[0].commander());
+                model.kickUser(index);
+                model.LegionCommander(index);
+            } else {
+                console.log("FUCK YEAH");
+            }
+        };
+
+        $('.army-button.btn_add_ai').parent().append('<div class="army-button btn_add_ai" data-bind="visible: slot.isEmpty(),click: function() { model.LegionCommander(army.index());}">Add Legion AI</div>');        
+        //END OF UNPATCHED lobby.js
 
 /*      var commanderObjectNameToHack = 'ImperialFiveleafclover';
 
@@ -41,6 +109,8 @@ if (!legionExpansionLoaded) {
         }
     }
 */
+
+
     }
      
     try {
@@ -50,67 +120,3 @@ if (!legionExpansionLoaded) {
         console.log(JSON.stringify(e));
     }
 }
-//LOAD CUSTOM LEGION NEW GAME CSS
-loadCSS("coui://ui/mods/com.pa.legion-expansion/css/new_game.css");
-loadScript("coui://ui/mods/com.pa.legion-expansion/common.js");
-
-var legioncommanders = legionglobal.commanders;
-var vanillacommanders = ["/pa/units/commanders/quad_osiris/quad_osiris.json","/pa/units/commanders/imperial_delta/imperial_delta.json"];
-
-model.isNotLegion = function(commander,isEmpty){
-  if(!isEmpty){
-    return !_.includes(legioncommanders, commander);
-  }
-  else{
-    return true;
-  }
-}
-
-//Style Commander Picker Legion
-$('.div-commander-picker-item.btn_std_ix').attr("data-bind","css: {legioncommander: !model.isNotLegion($data)}, click: function () { model.setCommander($index()) }, click_sound: 'default', rollover_sound: 'default'");
-
-//Style Slot Legion
-$('.slot-player').attr("data-bind","css: {legionslot: !model.isNotLegion($data.commander(),$data.isEmpty()), ready: isReady, loading: isLoading}");
-
-
-model.changeLegionAI = function(playerid){
-    //console.log("change to legion");
-    model.send_message('set_ai_commander', {
-        id: playerid,
-        ai_commander: legioncommanders[_.random(legioncommanders.length-1)]
-    });   
-}
-
-model.changeVanillaAI = function(playerid){
-    //console.log("change to vanilla");
-    model.send_message('set_ai_commander', {
-        id: playerid,
-        ai_commander: vanillacommanders[_.random(vanillacommanders.length-1)]
-    });   
-}
-
-//To Legion Button
-//$('.army-button.slot-remove-button.slot-remove-button-team').parent().append('<div class="army-button btn_add_ai" data-bind="visible: slot.ai() && model.isNotLegion(slot.commander()),click: function() { model.changeLegionAI(slot.playerId());}">to Legion</div>');
-//To Vanilla Button
-//$('.army-button.slot-remove-button.slot-remove-button-team').parent().append('<div class="army-button btn_add_ai" data-bind="visible: slot.ai() && !model.isNotLegion(slot.commander()),click: function() { model.changeVanillaAI(slot.playerId());}">to Vanilla</div>');
-
-
-//HACKY WAY TO SWITCH LEGION COMMANDER till lobby.js is updated
-model.LegionCommander = function (index) {
-    model.targetAIArmyIndex(index);
-    model.targetAISlotIndex(0);
-    model.addAI();
-    console.log(index);
-    setTimeout(model.CheckLegionCommander, 200, index);
-};
-model.CheckLegionCommander = function (index) {
-    if ((model.armies()[index].slots()[0].commander() !== "/pa/units/commanders/tank_aeson/tank_aeson.json") && (model.armies()[index].slots()[0].commander() !== "/pa/units/commanders/imperial_invictus/imperial_invictus.json")) {
-        console.log(model.armies()[index].slots()[0].commander());
-        model.kickUser(index);
-        model.LegionCommander(index);
-    } else {
-        console.log("FUCK YEAH");
-    }
-};
-
-$('.army-button.btn_add_ai').parent().append('<div class="army-button btn_add_ai" data-bind="visible: slot.isEmpty(),click: function() { model.LegionCommander(army.index());}">Add Legion AI</div>');
