02c3d67462606f51e5176ee7da700ba2d731cad6
elodea <elodens4@hotmail.com>
2016-02-28 14:36:03 +1100

    Revert "Applying master fix to balance."
    This reverts commit 2d44f4a77d3d84a453fab58e71d4bbdd639eef59.

--- split_into_mods.py
+++ split_into_mods.py
@@ -14,7 +14,7 @@ ROOT_PATH = './' # this expects the script to reside and be executed from within
 
 # this is regex, use it to select the files which you want to be in the client mod
 # this assumes that everything else is for the server mod
-CLIENT_FILES = ('.png', '_mask.papa', '_diffuse.papa', '_material.papa', '.fs')
+CLIENT_FILES = ('.png', '_mask.papa', '_diffuse.papa', '_material.papa', '.fs', '.pfx')
 
 CLIENT_SUFFIX = '-Client'
 SERVER_SUFFIX = '-Server'
