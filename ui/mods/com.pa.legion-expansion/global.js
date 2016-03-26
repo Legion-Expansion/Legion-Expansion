c757864fb23c82ae6b96b661fb44b9544112a011
KillerKiwiJuice <eoin@comcast.net>
2016-03-26 11:50:45 -0400

    manually reverts riot boat merge

--- ui/mods/com.pa.legion-expansion/global.js
+++ ui/mods/com.pa.legion-expansion/global.js
@@ -573,12 +573,6 @@
         preferred_builds: [
           ["L_sea", 16]
         ]
-      }, {
-        spec_id: "/pa/units/sea/L_boat_riot/L_boat_riot.json",
-        si_fallback: ["destroyer"],
-        preferred_builds: [
-          ["L_sea", 17]
-        ]
       },
 
 
