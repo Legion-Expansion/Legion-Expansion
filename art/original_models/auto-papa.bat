a535fb9c7b488f63003af40ce9413d698bfff2dd
KillerKiwiJuice <eoin@comcast.net>
2016-03-26 15:08:19 -0400

    added hover ship fixes #168
    IT'S ALMOST DONEEEEEE *maniacal laughter*

--- art/original_models/auto-papa.bat
+++ art/original_models/auto-papa.bat
@@ -1,5 +1,5 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
-set unit_to_convert=L_delta_v_engine
+set unit_to_convert=L_hover_ship
 papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%.fbx"
 papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_start.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_start.fbx"
 papatran --texture-mode strip -o "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_end.papa" "C:\Users\Eoin\AppData\Local\Uber Entertainment\Planetary Annihilation\server_mods\com.pa.legion-expansion.server\art\original_models\%unit_to_convert%_end.fbx"
--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=L_orbital_factory_
+set unit=L_hover_ship_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
