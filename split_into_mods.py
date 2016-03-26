860b934281e1e126d6d892f5660077e06a2af485
pieter vanh <pieter.vanh@gmail.com>
2016-03-27 00:59:08 +0100

    split_into_mods script update

--- split_into_mods.py
+++ split_into_mods.py
@@ -4,29 +4,15 @@ import os, errno
 import re
 import shutil
 
-############################
-## Configuration
-##   Edit this part to change which files are selected for client/server split
-############################
-
-# location of the mod (can be relative or absolute)
 ROOT_PATH = './' # this expects the script to reside and be executed from within the base mod folder
 
-# this is regex, use it to select the files which you want to be in the client mod
-# this assumes that everything else is for the server mod
-CLIENT_FILES = ('.png', '_mask.papa', '_diffuse.papa', '_material.papa', '.fs', 'server_browser.js','_client.js','_client.css','.html')
 
-CLIENT_SUFFIX = '-Client'
-SERVER_SUFFIX = '-Server'
+CLIENT_FILES = ('.png', '.papa', '.fs', 'server_browser.js', '_client.js', '_client.css', '.html', '.pfx')
+PAPA_CLIENT = ('L_energy_plant.papa', 'L_energy_plant_adv.papa', 'L_land_barrier.papa', 'L_mex.papa', 'L_mex_adv.papa', 'L_radar.papa', 'L_radar_adv.papa', 'L_storage.papa', 'L_orbital_factory.papa', 'L_naval_factory.papa', 'L_naval_factory_adv.papa')
 
 EXCLUDE_DIRECTORIES = ('.', '.git', 'art', 'modinfo')
 
-CLIENT_MODINFO = 'modinfo/client/modinfo.json'
-SERVER_MODINFO = 'modinfo/server/modinfo.json'
 
-################# Not configuration section :P
-
-# makes directory, doesn't care if it exists already
 def mkdir_p(path):
     try:
         os.makedirs(path)
@@ -34,31 +20,40 @@ def mkdir_p(path):
         if exc.errno == errno.EEXIST and os.path.isdir(path): pass
         else: raise
 
-root_folder = os.path.realpath(ROOT_PATH)
-print ('Mod folder: ' + root_folder)
+REAL_ROOT_PATH = os.path.realpath(ROOT_PATH)
+
+CLIENT_MODINFO = os.path.join(REAL_ROOT_PATH,'modinfo/client/modinfo.json')
+SERVER_MODINFO = os.path.join(REAL_ROOT_PATH,'modinfo/server/modinfo.json')
+
+print ('Mod folder: ' + REAL_ROOT_PATH)
 
 # iterate over all files
 for root, dirs, files in os.walk(ROOT_PATH):
     base_dir = os.path.normpath(root)
-
+    dirname = os.path.basename(base_dir);
+    
     if base_dir.startswith(EXCLUDE_DIRECTORIES): 
         continue
 
-    client_dir = os.path.join(root_folder + CLIENT_SUFFIX, base_dir)
-    server_dir = os.path.join(root_folder + SERVER_SUFFIX, base_dir)
+    CLIENT_DIR = os.path.join(REAL_ROOT_PATH + '-client', base_dir);
+    SERVER_DIR = os.path.join(REAL_ROOT_PATH + '-server', base_dir);
 
-    for f in files:
-        base_path = os.path.join(base_dir, f)
-        #print (base_path)
-        if base_path.endswith(CLIENT_FILES):
-            full_path = os.path.join(client_dir, f)
-            mkdir_p(client_dir)
+    for filename in files:
+        src = os.path.join(REAL_ROOT_PATH, base_dir, filename)
+        server_exception = (filename == ( dirname + '.papa' ) and not filename.endswith(PAPA_CLIENT))
+
+        if src.endswith(CLIENT_FILES) and not server_exception:
+            dest_root_path = CLIENT_DIR
         else:
-            full_path = os.path.join(server_dir, f)
-            mkdir_p(server_dir)
+            dest_root_path = SERVER_DIR
 
-        print ('Copying: ' + os.path.normpath(full_path))
-        shutil.copyfile(base_path, full_path)
+        dest_path = os.path.join(dest_root_path, base_dir)
+        mkdir_p(dest_path)
+        dest = os.path.join(dest_path, filename)
+        shutil.copyfile(src, dest)
+        
+shutil.copyfile( os.path.join(REAL_ROOT_PATH, 'modinfo/client/modinfo.json'), os.path.join( REAL_ROOT_PATH + '-client', 'modinfo.json') )
+#shutil.copyfile( os.path.join(REAL_ROOT_PATH, 'modinfo/client/test-modinfo.json'), os.path.join( REAL_ROOT_PATH + '-client', 'modinfo.json') )
 
-shutil.copyfile(os.path.join(root_folder, CLIENT_MODINFO), os.path.join(root_folder + CLIENT_SUFFIX, 'modinfo.json'))
-shutil.copyfile(os.path.join(root_folder, SERVER_MODINFO), os.path.join(root_folder + SERVER_SUFFIX, 'modinfo.json'))
+shutil.copyfile( os.path.join(REAL_ROOT_PATH, 'modinfo/server/modinfo.json'), os.path.join( REAL_ROOT_PATH + '-server', 'modinfo.json') )
+#shutil.copyfile( os.path.join(REAL_ROOT_PATH, 'modinfo/server/test-modinfo.json'), os.path.join( REAL_ROOT_PATH + '-server', 'modinfo.json') )
