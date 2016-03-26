37ba4a312b011d42c30f5c1b4e5f771213a3df63
pieter vanh <pieter.vanh@gmail.com>
2016-03-26 18:08:04 +0100

    Install script for merging shared

--- install_new.py
+++ install_new.py
@@ -3,6 +3,7 @@
 import os, errno
 import re
 import shutil
+import distutils.dir_util
 
 ############################
 ## Configuration
@@ -15,20 +16,21 @@ ROOT_PATH = './' # this expects the script to reside and be executed from within
 CLIENT_SUFFIX = '-Client'
 SERVER_SUFFIX = '-Server'
 
-################# Not configuration section :P
-
-# makes directory, doesn't care if it exists already
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path): pass
-        else: raise
-
 root_folder = os.path.realpath(ROOT_PATH)
 print ('Mod folder: ' + root_folder)
-
+try:
+    distutils.dir_util.remove_tree(os.path.join(root_folder + SERVER_SUFFIX))
+except:
+    print('No Old Server Folder')
+try:
+    distutils.dir_util.remove_tree(os.path.join(root_folder + CLIENT_SUFFIX))
+except:
+    print('No Old Client Folder')
+print ('Copy Client')
 shutil.copytree(os.path.join(root_folder, 'client'), os.path.join(root_folder + CLIENT_SUFFIX))
+print ('Copy Server')
 shutil.copytree(os.path.join(root_folder, 'server'), os.path.join(root_folder + SERVER_SUFFIX))
-shutil.copytree(os.path.join(root_folder, 'shared'), os.path.join(root_folder + CLIENT_SUFFIX))
-shutil.copytree(os.path.join(root_folder, 'shared'), os.path.join(root_folder + SERVER_SUFFIX))
+print ('Merge Shared into Client')
+distutils.dir_util.copy_tree(os.path.join(root_folder, 'shared'), os.path.join(root_folder + CLIENT_SUFFIX))
+print ('Merge Shared into Server')
+distutils.dir_util.copy_tree(os.path.join(root_folder, 'shared'), os.path.join(root_folder + SERVER_SUFFIX))
