7a9322d91529c18d323deca99df8265957112cdd
cptconundrum <conundrum@eprice.name>
2016-03-27 14:38:02 -0400

    One-time intro autoplay (youtube)
    Plays a youtube video once on game startup, then never again.

--- client/ui/mods/com.pa.legion-expansion/start.js
+++ client/ui/mods/com.pa.legion-expansion/start.js
@@ -28,3 +28,25 @@ if ( ! legionExpansionLoaded )
         console.log(JSON.stringify(e));
     }
 }
+
+// Play intro video one time only
+(function() {
+	var legion_intro_one_time = localStorage.legion_intro_one_time;
+
+	//load html dynamically
+	legion_loadHtmlTemplate = function(element, url) {
+		element.load(url, function () {
+			console.log("Loading html " + url);
+			element.children().each(function() {
+				ko.applyBindings(model, this);
+			});
+		});
+	};
+
+	if(legion_intro_one_time != "true") {
+	   $("body").append("<div id='legion_intro_wrapper'></div>");
+	   legion_loadHtmlTemplate($("#legion_intro_wrapper"), "coui://ui/mods/com.pa.legion-expansion/start/intro.html");
+		localStorage.legion_intro_one_time = "true";
+	}
+
+})();
