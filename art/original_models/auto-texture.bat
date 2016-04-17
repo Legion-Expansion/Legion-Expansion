716eb39d4de5bbd859a5c6eed0a2f44415aa90c7
KillerKiwiJuice <eoin@comcast.net>
2016-04-17 15:18:55 -0400

    mex texture update

--- art/original_models/auto-texture.bat
+++ art/original_models/auto-texture.bat
@@ -1,6 +1,6 @@
 cd C:\Program Files (x86)\Steam\SteamApps\common\Planetary Annihilation Titans\bin_x64\tools\
 
-set unit=l_tank_shank_
+set unit=l_mex_
 set diffuse=%unit%diffuse
 set material=%unit%material
 set mask=%unit%mask
