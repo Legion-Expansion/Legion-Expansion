cbff883fdb09a6177622f4e7d164c261b2018db6
cptconundrum <conundrum@eprice.name>
2016-04-17 21:56:47 -0400

    Revert "Better way to do intro video"
    This reverts commit c6e31682738a05eb251b6eb7f56af5ff37f893f3.

--- client/ui/mods/com.pa.legion-expansion/start.js
+++ client/ui/mods/com.pa.legion-expansion/start.js
@@ -35,19 +35,23 @@ if ( ! legionExpansionLoaded )
 // Play intro video one time only
 
 // BLOCKED UNTIL WE HAVE AN INTRO VIDEO
-var legion_intro_one_time = localStorage.legion_intro_one_time;
+/*var legion_intro_one_time = localStorage.legion_intro_one_time;
 
-if(legion_intro_one_time != "true ") {
+if(legion_intro_one_time != "true") {
 
-	var intro_url = "http://www.htmlcssvqs.com/8ed/examples/chapter-17/paddle-steamer.webm";
-
+	var intro_url = "https://www.youtube.com/embed/xGp4NClVm2I?autoplay=1";
 	$("body").append("<div id='legion_intro_wrapper'>" +
-	"<div id='legion_intro' style='position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: 11111; background-color: black;'>" +
-	"<video src='" + intro_url + "' width='100%' height='100%' autoplay=''></video>" +
+	"<div id='legion_intro' style='position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: 11111;'>" +
+	"	<iframe width='100%' height='100%' src='" + intro_url + "' frameborder='0' allowfullscreen></iframe>" +
+	"</div>" +
+	"<div id='legion_intro_top' style='position: fixed; left: 0; top: 0; width: 100%; height: 50px; z-index: 22222; padding-left: 1px; padding-right: 1px;'>" +
+	"	<div style='width: 100%; height: 100%; background-color: black;'>" +
+	"		<div style='position: fixed; right: 10px; top: 10px; width: 80px; height: 50px; z-index: 33333; background-image: url(coui://ui/main/shared/img/buttons/btn_lrg_std.png); text-align: center; padding-top: 8px; font-size: 20px;' onclick='$(\"#legion_intro\").remove();$(\"#legion_intro_top\").remove();'>Close</div>" +
+	"	</div>" +
 	"</div>" +
 	"</div>");
 	localStorage.legion_intro_one_time = "true";
 }
 
-
+*/
 
