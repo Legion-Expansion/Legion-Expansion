01acb3a6d0b68a77200e8d854c2b6b5fedda0e22
domdom <domdom314@gmail.com>
2016-09-10 11:32:19 +1000

    added weapon tracking directly to mod

---
+++ generate_weapon_tracking.py
@@ -0,0 +1,158 @@
+
+
+""" this script takes all the targets that are needed by legion for their shields and changes their effect paths """
+# tools for generating this mod
+import os
+import os.path as path
+import copy
+
+from datetime import datetime
+from shutil import copyfile
+
+from pa_tools.pa.load import Loader
+from pa_tools.pa import paths
+from pa_tools.pa import pajson
+
+print ('PA MEDIA DIR:', paths.PA_MEDIA_DIR)
+# create file resolution mappings (handles the mounting of pa_ex1 on pa and fallback etc.)
+loader = Loader(paths.PA_MEDIA_DIR)
+loader.mount('/', '/pa_ex1')
+
+
+with open("server/pa/units/land/l_shield_gen/anti_entity_targets.json",'r',encoding='utf8') as file:
+    targets, warnings = pajson.load(file)
+    targets = targets["anti_entity_targets"]
+
+# inlcude only PA and PAT units
+targets = list(filter(lambda target: loader.hasFile(target), targets))
+
+"""###################################################################################"""
+#########################################################################################
+"""###################################################################################"""
+
+# file cache for spec loading
+_cache = {}
+
+
+def _updateSpec(base_spec, spec):
+    base_spec = copy.deepcopy(base_spec)
+    for key, value in spec.items():
+        if key in base_spec and isinstance(base_spec[key], dict) and isinstance(value, dict):
+            base_spec[key] = _updateSpec(base_spec[key], value)
+        elif key in base_spec and base_spec[key] != value:
+            base_spec[key] = value
+        elif key not in base_spec:
+            base_spec[key] = value
+
+    return base_spec
+
+
+def _pruneSpec(spec, base_spec):
+    spec = copy.deepcopy(spec)
+    spec_keys = list(spec.keys() & base_spec.keys())
+    for key in spec_keys:
+        if key in base_spec:
+            if spec[key] == base_spec[key]:
+                del spec[key]
+            elif isinstance(spec[key], dict) and isinstance(base_spec[key], dict):
+                spec[key] = _pruneSpec(spec[key], base_spec[key])
+
+    return spec
+
+
+def _parseSpec(file_path):
+    # do cache lookup first
+    if file_path in _cache:
+        return _cache[file_path]
+
+    global loader
+    resolved_file_path = loader.resolveFile(file_path)
+
+    with open(resolved_file_path,'r',encoding='utf8') as file:
+        spec, warnings = pajson.load(file)
+        base_spec_id = spec.get('base_spec', None)
+        if base_spec_id:
+            base_spec = _parseSpec(base_spec_id)
+            spec = _updateSpec(base_spec, spec)
+
+    _cache[file_path] = spec
+    return copy.deepcopy(spec)
+
+
+"""###################################################################################"""
+#########################################################################################
+"""###################################################################################"""
+
+
+print (' ::Parsing ammo specs::')
+for target in targets:
+    print ('ammo:', target)
+    ammo_dir = path.dirname(target)
+    ammo_name = path.splitext(path.basename(target))[0]
+
+    # get the spec
+    ammo = _parseSpec(target)
+    ammo['physics']['add_to_spatial_db'] = True
+
+    # get the vanila effect that we are going to duplicate
+    src_trail_file = ammo['fx_trail']['filename']
+    src_hit_file = ammo['events']['died']['effect_spec']
+
+    # construct the new effect names relative to the location of the actual ammo file
+    dst_trail_file = path.join(ammo_dir, ammo_name + '_trail.pfx')
+    dst_hit_file = path.join(ammo_dir, ammo_name + '_hit.pfx')
+
+    ammo['fx_trail']['filename'] = dst_trail_file
+    ammo['events']['died']['effect_spec'] = dst_hit_file
+
+
+    # make a minimal ammo spec for the server mod
+    base_ammo = _parseSpec(ammo['base_spec'])
+    ammo = _pruneSpec(ammo, base_ammo)
+
+    # prepare files:
+    os.makedirs('server' + ammo_dir, exist_ok=True)
+    os.makedirs('client' + ammo_dir, exist_ok=True)
+
+    # copy client files
+    copyfile(loader.resolveFile(src_hit_file), 'client' + dst_hit_file)
+    copyfile(loader.resolveFile(src_trail_file), 'client' + dst_trail_file)
+
+    with open('server' + target, 'w', encoding='utf-8') as ammo_file:
+        pajson.dump(ammo, ammo_file)
+
+print (' ::Creating modinfo::')
+from collections import OrderedDict
+common = OrderedDict()
+
+with open(os.path.join(paths.PA_MEDIA_DIR, '../version.txt')) as f:
+    common['build'] = f.read().strip()
+
+common['author'] = 'dom314'
+common['date'] = datetime.utcnow().strftime("%Y-%m-%d")
+common['signature'] = ' '
+common['identifier'] = 'com.pa.domdom.weapons_tracking.'
+common['version'] = '0.0.1'
+# common['framework'] = True
+
+common['display_name'] = 'Weapons Tracking for Legion'
+common['description'] = 'Allows both projectile interception for legion, as well as effect overriding in other client mods.'
+
+client = copy.deepcopy(common)
+client['context'] = 'client'
+client['category'] = ['effects']
+client['display_name'] += ' Client'
+client['identifier'] += 'client'
+
+server = copy.deepcopy(common)
+server['context'] = 'server'
+server['category'] = ['units']
+server['display_name'] += ' Server'
+server['identifier'] += 'server'
+server['companions'] = [client['identifier']]
+
+
+with open('server/modinfo.json', 'w', encoding='utf-8') as file:
+    pajson.dump(server, file, indent=2)
+with open('client/modinfo.json', 'w', encoding='utf-8') as file:
+    pajson.dump(client, file, indent=2)
