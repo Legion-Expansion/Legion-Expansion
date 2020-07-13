c7faf2fe23740384a8bf8b8f89785eb99037da53
Quitch <Quitch@users.noreply.github.com>
2020-07-13 12:40:55 +0100

    Version it

--- client/ui/mods/com.pa.legion-expansion/live_game_options_bar.js
+++ client/ui/mods/com.pa.legion-expansion/live_game_options_bar.js
@@ -11,7 +11,7 @@ if (!legionExpansionLoaded) {
       ]).length > 0;
     if (legionServerLoaded) {
       $(".div_ingame_options_bar_cont").prepend(
-        "<div class='watermark'><loc>Legion</loc>&nbsp;#.##.#</div>"
+        "<div class='watermark'><loc>Legion</loc>&nbsp;1.15.3</div>"
       );
       locUpdateDocument();
     }
