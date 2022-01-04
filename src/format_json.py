5e93d86fcdf7bfbf8b3f2c2aa05c7010318d5bac
domdom <domdom@users.noreply.github.com>
2022-01-05 07:49:44 +1000

    format_json.py: refactor into function

--- src/format_json.py
+++ src/format_json.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python
+import os
+import sys
+
 from pathlib import Path
 
 from pa_tools.pa import pajson
-from pa_tools.pa.paths import PA_MEDIA_DIR
 
-# Format all json files in server and client
-file_list = []
-file_list.extend(Path("./server").glob("**/*.json"))
-file_list.extend(Path("./client").glob("**/*.json"))
-file_list.extend(Path("./server").glob("**/*.pfx"))
-file_list.extend(Path("./client").glob("**/*.pfx"))
+# Run from this script's directory
+os.chdir(os.path.dirname(sys.argv[0]))
 
-for file in file_list:
-    # normalize paths to posix
-    file = file.as_posix()
+def format_files(client_out_dir, server_out_dir):
+    # Format all json files in server and client
+    file_list = []
+    file_list.extend(Path(server_out_dir).glob("**/*.json"))
+    file_list.extend(Path(client_out_dir).glob("**/*.json"))
+    file_list.extend(Path(server_out_dir).glob("**/*.pfx"))
+    file_list.extend(Path(client_out_dir).glob("**/*.pfx"))
 
-    data, warnings = pajson.loadf(file)
-    list(map(print, warnings))
+    for file in file_list:
+        # normalize paths to posix
+        file = file.as_posix()
 
-    with open(file, "w") as f:
-        f.write(pajson.dumps(data, indent=2))
-        f.write("\n")
+        data, warnings = pajson.loadf(file)
+        list(map(print, warnings))
+
+        with open(file, "w", encoding="utf-8", newline='\n') as f:
+            pajson.dump(data, f, indent=2)
+            f.write("\n")
+
+if __name__ == "__main__":
+    format_files("../client", "../server")
