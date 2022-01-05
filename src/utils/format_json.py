3676f782882bafa2a5f554d0b68f436bf959093e
domdom <domdom@users.noreply.github.com>
2022-01-05 13:06:20 +1000

    move format_json.py to utils directory

---
+++ src/utils/format_json.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import os
+import sys
+
+from pathlib import Path
+
+from pa_tools.pa import pajson
+
+# Run from this script's directory
+os.chdir(os.path.dirname(sys.argv[0]))
+
+
+def format_files(directory):
+    # Format all json files in server and client
+    file_list = []
+    file_list.extend(Path(directory).glob("**/*.json"))
+    file_list.extend(Path(directory).glob("**/*.pfx"))
+
+    for file in file_list:
+        # normalize paths to posix
+        file = file.as_posix()
+
+        data, warnings = pajson.loadf(file)
+        list(map(print, warnings))
+
+        if file.endswith(".pfx"):
+            pajson.dumpf_effect(data, file, indent=2)
+        else:
+            pajson.dumpf(data, file, indent=2)
+
+
+if __name__ == "__main__":
+    format_files("..")
