3278f84383c54ef2650691c711b6f58d71fe12d2
Quitch <Quitch@users.noreply.github.com>
2022-08-05 09:03:05 +0100

    Move path creation into its own function
    Avoid duplication of code and literal strings

--- src/client/ui/mods/com.pa.legion-expansion/common_functions.js
+++ src/client/ui/mods/com.pa.legion-expansion/common_functions.js
@@ -11,13 +11,16 @@ define({
   imageSourceForType: function (path, colour, type) {
     return path + colour + "/icon_category_" + type.toLowerCase() + ".png";
   },
+  setupSourcePath: function (src, png) {
+    var prefixedSource = "img[src='" + src;
+    return prefixedSource + png + "']";
+  },
   toggleImage: function (src, path, colour, png1, png2) {
     var imgPath = path + colour + "/";
-    var prefixedSource = "img[src='" + src;
-    var src1 = prefixedSource + png1 + "']";
+    var src1 = this.setupSourcePath(src, png1);
     $(src1).attr("src", imgPath + png1);
     if (png2) {
-      var src2 = prefixedSource + png2 + "']";
+      var src2 = this.setupSourcePath(src, png2);
       $(src2).attr("src", imgPath + png2);
     }
   },
